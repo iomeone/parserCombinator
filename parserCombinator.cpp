284466152acddd427f2d9cb8e231ff66d5e8bdea
iomeone <46809140+iomeone@users.noreply.github.com>
2019-02-27 22:06:10 +0800

    Update parserCombinator.cpp

--- parserCombinator.cpp
+++ parserCombinator.cpp
@@ -1,5 +1,4 @@
-﻿// parserCombinator.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//  https://gist.github.com/swlaschin/485f418fede6b6a36d89#file-understanding_parser_combinators-3-fsx
+//coded by zhu fei (jellyzone@gmail.com), all rights reseverd
 
 #include <iostream>
 #include <sstream>
